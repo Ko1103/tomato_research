1d1e1bfe0c1c1da571bf6c24d6cf3bdf2df28bfb
KO-YAMAKO <ko.foot1103@gmail.com>
2018-09-28 13:50:09 +0900

    refrect gitignore

--- rail/slprj/_sfprj/rail_ver01/_self/sfun/src/rail_ver01_sfun.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files\MATLAB\R2018a\sys\lcc64\lcc64\bin\lccmake.exe" -f rail_ver01_sfun.lmk
--- rail/slprj/_sfprj/test_kinect_ver2/_self/sfun/src/test_kinect_ver2_sfun.bat
+++
@@ -1,1 +0,0 @@
-"C:\Program Files\MATLAB\R2018a\sys\lcc64\lcc64\bin\lccmake.exe" -f test_kinect_ver2_sfun.lmk
